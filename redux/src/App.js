58051655218b9045d9035a40c1b239051259839a
matheusfreitas777 <marth2518277@gmail.com>
2022-03-12 15:46:10 +0000

    Aula do dia 12/03

---
+++ redux/src/App.js
@@ -0,0 +1,21 @@
+import logo from './logo.svg';
+import './App.css';
+import Card from './componentes/Card'
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Projeto-Redux</h1> 
+    <div className='linha'>
+    <Card> </Card>
+    </div>
+
+    <div className='linha'>
+      <Card></Card>
+    </div>
+    </div>
+
+  );
+}
+
+export default App;
