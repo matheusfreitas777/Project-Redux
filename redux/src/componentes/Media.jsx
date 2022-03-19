f828859cb347535c67eef852cec95bd949106e38
matheusfreitas777 <marth2518277@gmail.com>
2022-03-19 15:42:45 +0000

    Aula 19/03

---
+++ redux/src/componentes/Media.jsx
@@ -0,0 +1,16 @@
+import React from "react"
+import Card from "./Card"
+
+export default props => {
+    const {min, max} = props
+    return (
+        <Card title="Média dos Números" green>
+            <div>
+                <span>
+                    <span>Resultado: </span>
+                    <strong>{(min + max)/2}</strong>
+                </span>
+            </div>
+        </Card>
+    )
+}
