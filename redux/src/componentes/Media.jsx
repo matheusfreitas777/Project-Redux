59effea0fd285cf98b843969c1a93744c27752e0
matheusfreitas777 <marth2518277@gmail.com>
2022-03-26 15:49:22 +0000

    Aula do dia 26/03

--- redux/src/componentes/Media.jsx
+++ redux/src/componentes/Media.jsx
@@ -1,16 +1,25 @@
 import React from "react"
 import Card from "./Card"
 
-export default props => {
+function Media(props){
     const {min, max} = props
     return (
         <Card title="Média dos Números" green>
             <div>
                 <span>
                     <span>Resultado: </span>
-                    <strong>{(min + max)/2}</strong>
+                    <span>{(min + max)/2}</span>
                 </span>
             </div>
         </Card>
     )
 }
+
+function mapStateToProps(state) {
+    return {
+        min: state.numeros.min,
+        max: state.numeros.max
+    }
+}
+
+export default connect(mapStateToProps)(Media)
--- redux/src/componentes/Soma.jsx
+++ redux/src/componentes/Soma.jsx
@@ -1,16 +1,25 @@
 import React from "react"
 import Card from "./Card"
 
-export default props => {
+function Soma(props){
     const {min, max} = props
     return (
         <Card title="Soma dos Números" blue>
             <div>
                 <span>
                     <span>Resultado: </span>
-                    <span>{min + max}</span>
+                    <span>{(min + max)}</span>
                 </span>
             </div>
         </Card>
     )
 }
+
+function mapStateToProps(state) {
+    return {
+        min: state.numeros.min,
+        max: state.numeros.max
+    }
+}
+
+export default connect(mapStateToProps)(Soma)
